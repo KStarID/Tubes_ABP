45baf729510673aec088e82abf92c46d9976ccb4
rama <88663800+amarkng@users.noreply.github.com>
2024-03-28 21:51:12 +0700

    Edit AddCars, dll

--- app/Http/Controllers/Firebase/CarsController.php
+++ app/Http/Controllers/Firebase/CarsController.php
@@ -32,15 +32,25 @@ class CarsController extends Controller
 
     public function store(Request $request)
     {
+        $email_penjual = auth()->user()->email;
+
         $post_data = [
             'merk' => $request->merk,
-            'nama' => $request->nama,
+            'model' => $request->model,
             'harga' => $request->harga,
+            'tahun_pembuatan' => $request->tahun_pembuatan,
+            'kondisi' => $request->kondisi,
+            'bahan_bakar' => $request->bahan_bakar,
+            'transmisi' => $request->transmisi,
+            'warna' => $request->warna,
+            'deskripsi' => $request->deskripsi,
+            'kontak_penjual' => $request->kontak_penjual,
+            'email_penjual' => $email_penjual,
         ];
         $postRef = $this->database->getReference($this->tablename)->push($post_data);
         if ($postRef) {
             Session::flash('message', 'New Cars Created');
-            return back()->withInput()->with('status', 'success');
+            return back()->withInput()->with('status', 'Success');
         } else {
             return redirect('/home/admin')->with('status', 'error');
         }
