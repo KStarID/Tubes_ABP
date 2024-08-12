8691f4773d86ffbcf0820d94ad694e5b97f51bee
KStarID <kemal.aziz03@gmail.com>
2024-08-12 21:49:55 +0700

    coba

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite('resources\css\app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ config('BaroCars', 'BaroCars') }}</title>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
     <script>
