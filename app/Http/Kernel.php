95640a8e491e5821a9f698242abe1c8b9639d38a
KStarID <kemal.aziz03@gmail.com>
2024-08-15 12:51:57 +0700

    deploy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,6 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
-            \App\Http\Middleware\EnsureHttps::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
