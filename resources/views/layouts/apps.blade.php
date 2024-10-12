c787a11453d0a2710bffbb52a59a098fb627fdd0
KStarID <kemal.aziz03@gmail.com>
2024-10-12 18:35:55 +0700

    fix

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -115,8 +115,9 @@
                             @csrf
                         </form>
                     @endguest
+                    <!-- Open Main Menu on the right -->
                     <button data-collapse-toggle="navbar-sticky" type="button"
-                        class="inline-flex md:hidden items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden lg:hidden xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-sticky" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -127,11 +128,10 @@
                     </button>
                 </div>
 
-                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                    <!-- Right Side Of Navbar -->
+                <!-- Menu dropdown untuk layar kecil -->
+                <div class="items-center justify-between hidden w-full md:hidden lg:hidden xl:hidden" id="navbar-sticky">
                     <ul
-                        class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <!-- Authentication Links -->
+                        class="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
                         @yield('navbar_home')
                     </ul>
                 </div>
