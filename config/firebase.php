283249365f897869cf78d17700f5918405446a9c
KStarID <kemal.aziz03@gmail.com>
2024-12-28 10:37:24 +0700

    remove firebase credentials and add to gitignore

---
+++ config/firebase.php
@@ -0,0 +1,12 @@
+'credentials' => [
+'type' => env('FIREBASE_TYPE'),
+'project_id' => env('FIREBASE_PROJECT_ID'),
+'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
+'private_key' => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
+'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+'client_id' => env('FIREBASE_CLIENT_ID'),
+'auth_uri' => env('FIREBASE_AUTH_URI'),
+'token_uri' => env('FIREBASE_TOKEN_URI'),
+'auth_provider_x509_cert_url' => env('FIREBASE_AUTH_PROVIDER_CERT_URL'),
+'client_x509_cert_url' => env('FIREBASE_CLIENT_CERT_URL'),
+],
